844e982963feae19f305299957c5b5ede5b04273
Przemek Grochowski <sovvly@protonmail.com>
2019-02-06 17:15:01 +0100

    Targetting netcore2.2
    Added targeting to .netcore2.2

--- FlatMoniApi.Tests/Services/MeasureServiceTest.cs
+++ FlatMoniApi.Tests/Services/MeasureServiceTest.cs
@@ -2,18 +2,18 @@
 using FlatMoni.Core.Domain;
 using FlatMoni.Core.Repositories;
 using FlatMoniApi.FlatMoni.Infrastructure.Services;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using System;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace FlatMoniApi.Tests.Services
 {
-    [TestClass]
+    
     public class MeasureServiceTest
     {
 
-        [TestMethod]
+        [Fact]
         public async Task MesureTest()
         {
             //measure mock 
